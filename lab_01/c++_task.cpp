550246b088cf1e4494977e3261179a106d09f434
qb1kkk <129790257+qb1kkk@users.noreply.github.com>
2025-09-26 10:15:57 +0300

    Update c++_task.cpp

--- lab_01/c++_task.cpp
+++ lab_01/c++_task.cpp
@@ -1,9 +1,28 @@
-//С++
+//Статический массив
 #include <iostream>
 using namespace std;
 
 int main() {
-    int arr[] = {1, 2, 3, 4, 5};
-    cout << arr << endl;
+    int arr[5] = {1, 2, 3, 4, 5};
+    
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+    
+    return 0;
+}
+
+//Динамический массив
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5};
+    
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    
     return 0;
 }
