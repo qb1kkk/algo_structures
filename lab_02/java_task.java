302fe9e7fb9a8e07b52b96216271461400a8dcbb
qb1kkk <129790257+qb1kkk@users.noreply.github.com>
2025-09-26 10:22:19 +0300

    Update java_task.java

--- lab_02/java_task.java
+++ lab_02/java_task.java
@@ -1,1 +1,59 @@
+//1 Пример
+class Node {
+    int data;
+    Node next;
+    
+    Node(int data) {
+        this.data = data;
+    }
+}
 
+public class Main {
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+    }
+}
+
+
+//2 пример
+class LinkedList {
+    class Node {
+        int data;
+        Node next;
+        
+        Node(int data) {
+            this.data = data;
+        }
+    }
+    
+    Node head;
+    
+    void add(int value) {
+        Node newNode = new Node(value);
+        newNode.next = head;
+        head = newNode;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.add(3);
+        list.add(2);
+        list.add(1);
+        
+        LinkedList.Node current = list.head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+    }
+}
