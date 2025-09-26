909a1107dab0ac35bbbec871a5570c8ac29bbf00
qb1kkk <129790257+qb1kkk@users.noreply.github.com>
2025-09-26 10:17:01 +0300

    Update java_task.java

--- lab_01/java_task.java
+++ lab_01/java_task.java
@@ -1,8 +1,26 @@
-import java.util.Arrays;
-
+//Статический массив
 public class Main {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
-        System.out.println(arr);
+        
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
+//Динамический массив
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.print(arr.get(i) + " ");
+        }
     }
 }
